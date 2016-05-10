34776851ed6919ccd1de74b7ebfdaab496879efb
David Luecke <daff@neyeon.com>
2016-05-10 14:15:52 -0700

    3.0.0-pre.4

---
+++ dist/cjs/helpers/core.js
@@ -0,0 +1,208 @@
+/*can-stache@3.0.0-pre.3#helpers/core*/
+var live = require('can-view-live');
+var nodeLists = require('can-view-nodelist');
+var compute = require('can-compute');
+var utils = require('../src/utils.js');
+var types = require('can-util/js/types/types');
+var isFunction = require('can-util/js/is-function/is-function');
+var each = require('can-util/js/each/each');
+var assign = require('can-util/js/assign/assign');
+var domData = require('can-util/dom/data/data');
+var resolve = function (value) {
+    if (types.isListLike(value) && value.attr('length')) {
+        return value;
+    } else if (isFunction(value)) {
+        return value();
+    } else {
+        return value;
+    }
+};
+var resolveHash = function (hash) {
+    var params = {};
+    for (var prop in hash) {
+        var value = hash[prop];
+        if (value && value.isComputed) {
+            params[prop] = value();
+        } else {
+            params[prop] = value;
+        }
+    }
+    return params;
+};
+var helpers = {
+    'each': function (items, options) {
+        var resolved = resolve(items), result = [], keys, key, i;
+        if (types.isListLike(resolved)) {
+            return function (el) {
+                var nodeList = [el];
+                nodeList.expression = 'live.list';
+                nodeLists.register(nodeList, null, options.nodeList, true);
+                nodeLists.update(options.nodeList, [el]);
+                var cb = function (item, index, parentNodeList) {
+                    return options.fn(options.scope.add({
+                        '%index': index,
+                        '@index': index
+                    }, { notContext: true }).add(item), options.options, parentNodeList);
+                };
+                live.list(el, items, cb, options.context, el.parentNode, nodeList, function (list, parentNodeList) {
+                    return options.inverse(options.scope.add(list), options.options, parentNodeList);
+                });
+            };
+        }
+        var expr = resolved;
+        if (!!expr && utils.isArrayLike(expr)) {
+            for (i = 0; i < expr.length; i++) {
+                result.push(options.fn(options.scope.add({
+                    '%index': i,
+                    '@index': i
+                }, { notContext: true }).add(expr[i])));
+            }
+        } else if (types.isMapLike(expr)) {
+            keys = expr.constructor.keys(expr);
+            for (i = 0; i < keys.length; i++) {
+                key = keys[i];
+                result.push(options.fn(options.scope.add({
+                    '%key': key,
+                    '@key': key
+                }, { notContext: true }).add(expr[key])));
+            }
+        } else if (expr instanceof Object) {
+            for (key in expr) {
+                result.push(options.fn(options.scope.add({
+                    '%key': key,
+                    '@key': key
+                }, { notContext: true }).add(expr[key])));
+            }
+        }
+        return result;
+    },
+    '@index': function (offset, options) {
+        if (!options) {
+            options = offset;
+            offset = 0;
+        }
+        var index = options.scope.attr('@index');
+        return '' + ((isFunction(index) ? index() : index) + offset);
+    },
+    'if': function (expr, options) {
+        var value;
+        if (isFunction(expr)) {
+            value = compute.truthy(expr)();
+        } else {
+            value = !!resolve(expr);
+        }
+        if (value) {
+            return options.fn(options.scope || this);
+        } else {
+            return options.inverse(options.scope || this);
+        }
+    },
+    'is': function () {
+        var lastValue, curValue, options = arguments[arguments.length - 1];
+        if (arguments.length - 2 <= 0) {
+            return options.inverse();
+        }
+        var args = arguments;
+        var callFn = compute(function () {
+            for (var i = 0; i < args.length - 1; i++) {
+                curValue = resolve(args[i]);
+                curValue = isFunction(curValue) ? curValue() : curValue;
+                if (i > 0) {
+                    if (curValue !== lastValue) {
+                        return false;
+                    }
+                }
+                lastValue = curValue;
+            }
+            return true;
+        });
+        return callFn() ? options.fn() : options.inverse();
+    },
+    'eq': function () {
+        return helpers.is.apply(this, arguments);
+    },
+    'unless': function (expr, options) {
+        return helpers['if'].apply(this, [
+            expr,
+            assign(assign({}, options), {
+                fn: options.inverse,
+                inverse: options.fn
+            })
+        ]);
+    },
+    'with': function (expr, options) {
+        var ctx = expr;
+        expr = resolve(expr);
+        if (!!expr) {
+            return options.fn(ctx);
+        }
+    },
+    'log': function (expr, options) {
+        if (typeof console !== 'undefined' && console.log) {
+            if (!options) {
+                console.log(expr.context);
+            } else {
+                console.log(expr, options.context);
+            }
+        }
+    },
+    'data': function (attr) {
+        var data = arguments.length === 2 ? this : arguments[1];
+        return function (el) {
+            domData.set.call(el, attr, data || this.context);
+        };
+    },
+    'switch': function (expression, options) {
+        resolve(expression);
+        var found = false;
+        var newOptions = options.helpers.add({
+            'case': function (value, options) {
+                if (!found && resolve(expression) === resolve(value)) {
+                    found = true;
+                    return options.fn(options.scope || this);
+                }
+            },
+            'default': function (options) {
+                if (!found) {
+                    return options.fn(options.scope || this);
+                }
+            }
+        });
+        return options.fn(options.scope, newOptions);
+    }
+};
+helpers.eachOf = helpers.each;
+var registerHelper = function (name, callback) {
+    helpers[name] = callback;
+};
+var makeSimpleHelper = function (fn) {
+    return function () {
+        var realArgs = [];
+        each(arguments, function (val, i) {
+            if (i <= arguments.length) {
+                while (val && val.isComputed) {
+                    val = val();
+                }
+                realArgs.push(val);
+            }
+        });
+        return fn.apply(this, realArgs);
+    };
+};
+module.exports = {
+    registerHelper: registerHelper,
+    registerSimpleHelper: function (name, callback) {
+        registerHelper(name, makeSimpleHelper(callback));
+    },
+    getHelper: function (name, options) {
+        var helper = options && options.get('helpers.' + name, { proxyMethods: false });
+        if (!helper) {
+            helper = helpers[name];
+        }
+        if (helper) {
+            return { fn: helper };
+        }
+    },
+    resolve: resolve,
+    resolveHash: resolveHash
+};
---
+++ dist/cjs/src/mustache_core.js
@@ -0,0 +1,246 @@
+/*can-stache@3.0.0-pre.3#src/mustache_core*/
+var live = require('can-view-live');
+var nodeLists = require('can-view-nodelist');
+var Scope = require('can-view-scope');
+var compute = require('can-compute');
+var ObserveInfo = require('can-observe-info');
+var utils = require('./utils.js');
+var expression = require('./expression.js');
+var types = require('can-util/js/types/types');
+var getDocument = require('can-util/dom/document/document');
+var frag = require('can-util/dom/frag/frag');
+var attr = require('can-util/dom/attr/attr');
+var mustacheLineBreakRegExp = /(?:(?:^|(\r?)\n)(\s*)(\{\{([^\}]*)\}\}\}?)([^\S\n\r]*)($|\r?\n))|(\{\{([^\}]*)\}\}\}?)/g, getItemsFragContent = function (items, isObserveList, helperOptions, options) {
+        var frag = getDocument().createDocumentFragment();
+        for (var i = 0, len = items.length; i < len; i++) {
+            append(frag, helperOptions.fn(isObserveList ? items.attr('' + i) : items[i], options));
+        }
+        return frag;
+    }, append = function (frag, content) {
+        if (content) {
+            frag.appendChild(typeof content === 'string' ? frag.ownerDocument.createTextNode(content) : content);
+        }
+    }, getItemsStringContent = function (items, isObserveList, helperOptions, options) {
+        var txt = '';
+        for (var i = 0, len = items.length; i < len; i++) {
+            txt += helperOptions.fn(isObserveList ? items.attr('' + i) : items[i], options);
+        }
+        return txt;
+    }, k = function () {
+    };
+var core = {
+    expression: expression,
+    makeEvaluator: function (scope, helperOptions, nodeList, mode, exprData, truthyRenderer, falseyRenderer, stringOnly) {
+        if (mode === '^') {
+            var temp = truthyRenderer;
+            truthyRenderer = falseyRenderer;
+            falseyRenderer = temp;
+        }
+        var value, helperOptionArg;
+        if (exprData instanceof expression.Call) {
+            helperOptionArg = {
+                fn: function () {
+                },
+                inverse: function () {
+                },
+                context: scope.attr('.'),
+                scope: scope,
+                nodeList: nodeList,
+                exprData: exprData,
+                helpersScope: helperOptions
+            };
+            utils.convertToScopes(helperOptionArg, scope, helperOptions, nodeList, truthyRenderer, falseyRenderer);
+            value = exprData.value(scope, helperOptions, helperOptionArg);
+            if (exprData.isHelper) {
+                return value;
+            }
+        } else {
+            var readOptions = {
+                isArgument: true,
+                args: [
+                    scope.attr('.'),
+                    scope
+                ],
+                asCompute: true
+            };
+            var helperAndValue = exprData.helperAndValue(scope, helperOptions, readOptions, nodeList, truthyRenderer, falseyRenderer, stringOnly);
+            var helper = helperAndValue.helper;
+            value = helperAndValue.value;
+            if (helper) {
+                return exprData.evaluator(helper, scope, helperOptions, readOptions, nodeList, truthyRenderer, falseyRenderer, stringOnly);
+            }
+        }
+        if (!mode) {
+            if (value && value.isComputed) {
+                return value;
+            } else {
+                return function () {
+                    return '' + (value != null ? value : '');
+                };
+            }
+        } else if (mode === '#' || mode === '^') {
+            helperOptionArg = {
+                fn: function () {
+                },
+                inverse: function () {
+                }
+            };
+            utils.convertToScopes(helperOptionArg, scope, helperOptions, nodeList, truthyRenderer, falseyRenderer);
+            return function () {
+                var finalValue;
+                if (types.isCompute(value)) {
+                    finalValue = value();
+                } else {
+                    finalValue = value;
+                }
+                if (typeof finalValue === 'function') {
+                    return finalValue;
+                } else if (typeof finalValue !== 'string' && utils.isArrayLike(finalValue)) {
+                    var isObserveList = types.isMapLike(finalValue);
+                    if (isObserveList ? finalValue.attr('length') : finalValue.length) {
+                        return (stringOnly ? getItemsStringContent : getItemsFragContent)(finalValue, isObserveList, helperOptionArg, helperOptions);
+                    } else {
+                        return helperOptionArg.inverse(scope, helperOptions);
+                    }
+                } else {
+                    return finalValue ? helperOptionArg.fn(finalValue || scope, helperOptions) : helperOptionArg.inverse(scope, helperOptions);
+                }
+            };
+        } else {
+        }
+    },
+    makeLiveBindingPartialRenderer: function (partialName, state) {
+        partialName = partialName.trim();
+        return function (scope, options, parentSectionNodeList) {
+            var nodeList = [this];
+            nodeList.expression = '>' + partialName;
+            nodeLists.register(nodeList, null, parentSectionNodeList || true, state.directlyNested);
+            var partialFrag = compute(function () {
+                var localPartialName = partialName;
+                var partial = options.attr('partials.' + localPartialName), renderer;
+                if (partial) {
+                    renderer = function () {
+                        return partial.render ? partial.render(scope, options, nodeList) : partial(scope, options);
+                    };
+                } else {
+                    var scopePartialName = scope.read(localPartialName, { isArgument: true }).value;
+                    if (scopePartialName === null || !scopePartialName && localPartialName[0] === '*') {
+                        return frag('');
+                    }
+                    if (scopePartialName) {
+                        localPartialName = scopePartialName;
+                    }
+                    renderer = function () {
+                        if (typeof localPartialName === 'function') {
+                            return localPartialName(scope, options, nodeList);
+                        } else {
+                            return core.getTemplateById(localPartialName)(scope, options, nodeList);
+                        }
+                    };
+                }
+                var res = ObserveInfo.notObserve(renderer)();
+                return frag(res);
+            });
+            partialFrag.computeInstance.setPrimaryDepth(nodeList.nesting);
+            live.html(this, partialFrag, this.parentNode, nodeList);
+        };
+    },
+    makeStringBranchRenderer: function (mode, expressionString) {
+        var exprData = core.expression.parse(expressionString), fullExpression = mode + expressionString;
+        if (!(exprData instanceof expression.Helper) && !(exprData instanceof expression.Call)) {
+            exprData = new expression.Helper(exprData, [], {});
+        }
+        return function branchRenderer(scope, options, truthyRenderer, falseyRenderer) {
+            var evaluator = scope.__cache[fullExpression];
+            if (mode || !evaluator) {
+                evaluator = makeEvaluator(scope, options, null, mode, exprData, truthyRenderer, falseyRenderer, true);
+                if (!mode) {
+                    scope.__cache[fullExpression] = evaluator;
+                }
+            }
+            var res = evaluator();
+            return res == null ? '' : '' + res;
+        };
+    },
+    makeLiveBindingBranchRenderer: function (mode, expressionString, state) {
+        var exprData = core.expression.parse(expressionString);
+        if (!(exprData instanceof expression.Helper) && !(exprData instanceof expression.Call)) {
+            exprData = new expression.Helper(exprData, [], {});
+        }
+        return function branchRenderer(scope, options, parentSectionNodeList, truthyRenderer, falseyRenderer) {
+            var nodeList = [this];
+            nodeList.expression = expressionString;
+            nodeLists.register(nodeList, null, parentSectionNodeList || true, state.directlyNested);
+            var evaluator = makeEvaluator(scope, options, nodeList, mode, exprData, truthyRenderer, falseyRenderer, state.tag);
+            var gotCompute = evaluator.isComputed, computeValue;
+            if (gotCompute) {
+                computeValue = evaluator;
+            } else {
+                computeValue = compute(evaluator, null, false);
+            }
+            computeValue.computeInstance.setPrimaryDepth(nodeList.nesting);
+            computeValue.computeInstance.bind('change', k);
+            var value = computeValue();
+            if (typeof value === 'function') {
+                ObserveInfo.notObserve(value)(this);
+            } else if (gotCompute || computeValue.computeInstance.hasDependencies) {
+                if (state.attr) {
+                    live.attr(this, state.attr, computeValue);
+                } else if (state.tag) {
+                    live.attrs(this, computeValue);
+                } else if (state.text && typeof value !== 'object') {
+                    live.text(this, computeValue, this.parentNode, nodeList);
+                } else {
+                    live.html(this, computeValue, this.parentNode, nodeList);
+                }
+            } else {
+                if (state.attr) {
+                    attr.set(this, state.attr, value);
+                } else if (state.tag) {
+                    live.attrs(this, value);
+                } else if (state.text && typeof value === 'string') {
+                    this.nodeValue = value;
+                } else if (value != null) {
+                    nodeLists.replace([this], frag(value, this.ownerDocument));
+                }
+            }
+            computeValue.computeInstance.unbind('change', k);
+        };
+    },
+    splitModeFromExpression: function (expression, state) {
+        expression = expression.trim();
+        var mode = expression.charAt(0);
+        if ('#/{&^>!'.indexOf(mode) >= 0) {
+            expression = expression.substr(1).trim();
+        } else {
+            mode = null;
+        }
+        if (mode === '{' && state.node) {
+            mode = null;
+        }
+        return {
+            mode: mode,
+            expression: expression
+        };
+    },
+    cleanLineEndings: function (template) {
+        return template.replace(mustacheLineBreakRegExp, function (whole, returnBefore, spaceBefore, special, expression, spaceAfter, returnAfter, spaceLessSpecial, spaceLessExpression, matchIndex) {
+            spaceAfter = spaceAfter || '';
+            returnBefore = returnBefore || '';
+            spaceBefore = spaceBefore || '';
+            var modeAndExpression = splitModeFromExpression(expression || spaceLessExpression, {});
+            if (spaceLessSpecial || '>{'.indexOf(modeAndExpression.mode) >= 0) {
+                return whole;
+            } else if ('^#!/'.indexOf(modeAndExpression.mode) >= 0) {
+                return special + (matchIndex !== 0 && returnAfter.length ? returnBefore + '\n' : '');
+            } else {
+                return spaceBefore + special + spaceAfter + (spaceBefore.length || matchIndex !== 0 ? returnBefore + '\n' : '');
+            }
+        });
+    },
+    Options: utils.Options,
+    getTemplateById: function () {
+    }
+};
+var makeEvaluator = core.makeEvaluator, splitModeFromExpression = core.splitModeFromExpression;
+module.exports = core;
