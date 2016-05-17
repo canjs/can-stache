b13dd45ca07a2bb30bdaa8ef941782dc24d14851
Justin Meyer <justinbmeyer@gmail.com>
2016-05-17 11:08:12 -0500

    3.0.0-pre.5

---
+++ dist/cjs/src/html_section.js
@@ -0,0 +1,121 @@
+/*can-stache@3.0.0-pre.4#src/html_section*/
+var target = require('can-view-target');
+var Scope = require('can-view-scope');
+var utils = require('./utils.js');
+var mustacheCore = require('./mustache_core.js');
+var getDocument = require('can-util/dom/document/document');
+var assign = require('can-util/js/assign/assign');
+var last = require('can-util/js/last/last');
+var decodeHTML = typeof document !== 'undefined' && function () {
+    var el = getDocument().createElement('div');
+    return function (html) {
+        if (html.indexOf('&') === -1) {
+            return html.replace(/\r\n/g, '\n');
+        }
+        el.innerHTML = html;
+        return el.childNodes.length === 0 ? '' : el.childNodes.item(0).nodeValue;
+    };
+}();
+var HTMLSectionBuilder = function () {
+    this.stack = [new HTMLSection()];
+};
+assign(HTMLSectionBuilder.prototype, utils.mixins);
+assign(HTMLSectionBuilder.prototype, {
+    startSubSection: function (process) {
+        var newSection = new HTMLSection(process);
+        this.stack.push(newSection);
+        return newSection;
+    },
+    endSubSectionAndReturnRenderer: function () {
+        if (this.last().isEmpty()) {
+            this.stack.pop();
+            return null;
+        } else {
+            var htmlSection = this.endSection();
+            return htmlSection.compiled.hydrate.bind(htmlSection.compiled);
+        }
+    },
+    startSection: function (process) {
+        var newSection = new HTMLSection(process);
+        this.last().add(newSection.targetCallback);
+        this.stack.push(newSection);
+    },
+    endSection: function () {
+        this.last().compile();
+        return this.stack.pop();
+    },
+    inverse: function () {
+        this.last().inverse();
+    },
+    compile: function () {
+        var compiled = this.stack.pop().compile();
+        return function (scope, options, nodeList) {
+            if (!(scope instanceof Scope)) {
+                scope = Scope.refsScope().add(scope || {});
+            }
+            if (!(options instanceof mustacheCore.Options)) {
+                options = new mustacheCore.Options(options || {});
+            }
+            return compiled.hydrate(scope, options, nodeList);
+        };
+    },
+    push: function (chars) {
+        this.last().push(chars);
+    },
+    pop: function () {
+        return this.last().pop();
+    }
+});
+var HTMLSection = function (process) {
+    this.data = 'targetData';
+    this.targetData = [];
+    this.targetStack = [];
+    var self = this;
+    this.targetCallback = function (scope, options, sectionNode) {
+        process.call(this, scope, options, sectionNode, self.compiled.hydrate.bind(self.compiled), self.inverseCompiled && self.inverseCompiled.hydrate.bind(self.inverseCompiled));
+    };
+};
+assign(HTMLSection.prototype, {
+    inverse: function () {
+        this.inverseData = [];
+        this.data = 'inverseData';
+    },
+    push: function (data) {
+        this.add(data);
+        this.targetStack.push(data);
+    },
+    pop: function () {
+        return this.targetStack.pop();
+    },
+    add: function (data) {
+        if (typeof data === 'string') {
+            data = decodeHTML(data);
+        }
+        if (this.targetStack.length) {
+            last(this.targetStack).children.push(data);
+        } else {
+            this[this.data].push(data);
+        }
+    },
+    compile: function () {
+        this.compiled = target(this.targetData, getDocument());
+        if (this.inverseData) {
+            this.inverseCompiled = target(this.inverseData, getDocument());
+            delete this.inverseData;
+        }
+        this.targetStack = this.targetData = null;
+        return this.compiled;
+    },
+    children: function () {
+        if (this.targetStack.length) {
+            return last(this.targetStack).children;
+        } else {
+            return this[this.data];
+        }
+    },
+    isEmpty: function () {
+        return !this.targetData.length;
+    }
+});
+HTMLSectionBuilder.HTMLSection = HTMLSection;
+module.exports = HTMLSectionBuilder;
