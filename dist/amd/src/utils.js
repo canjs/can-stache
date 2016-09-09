f95908b412da11cfaf50ef568f36d25d1dfaaf8e
Justin Meyer <justinbmeyer@gmail.com>
2016-09-08 20:02:00 -0500

    3.0.0-pre.20

---
+++ dist/amd/src/utils.js
@@ -0,0 +1,64 @@
+/*can-stache@3.0.0-pre.19#src/utils*/
+define(function (require, exports, module) {
+    var Scope = require('can-view-scope');
+    var Observation = require('can-observation');
+    var isArrayLike = require('can-util/js/is-array-like');
+    var Options = Scope.Options;
+    module.exports = {
+        isArrayLike: isArrayLike,
+        emptyHandler: function () {
+        },
+        jsonParse: function (str) {
+            if (str[0] === '\'') {
+                return str.substr(1, str.length - 2);
+            } else if (str === 'undefined') {
+                return undefined;
+            } else {
+                return JSON.parse(str);
+            }
+        },
+        mixins: {
+            last: function () {
+                return this.stack[this.stack.length - 1];
+            },
+            add: function (chars) {
+                this.last().add(chars);
+            },
+            subSectionDepth: function () {
+                return this.stack.length - 1;
+            }
+        },
+        convertToScopes: function (helperOptions, scope, options, nodeList, truthyRenderer, falseyRenderer, isStringOnly) {
+            if (truthyRenderer) {
+                helperOptions.fn = this.makeRendererConvertScopes(truthyRenderer, scope, options, nodeList, isStringOnly);
+            }
+            if (falseyRenderer) {
+                helperOptions.inverse = this.makeRendererConvertScopes(falseyRenderer, scope, options, nodeList, isStringOnly);
+            }
+        },
+        makeRendererConvertScopes: function (renderer, parentScope, parentOptions, nodeList, observeObservables) {
+            var rendererWithScope = function (ctx, opts, parentNodeList) {
+                return renderer(ctx || parentScope, opts, parentNodeList);
+            };
+            var convertedRenderer = function (newScope, newOptions, parentNodeList) {
+                if (newScope !== undefined && !(newScope instanceof Scope)) {
+                    newScope = parentScope.add(newScope);
+                }
+                if (newOptions !== undefined && !(newOptions instanceof Options)) {
+                    newOptions = parentOptions.add(newOptions);
+                }
+                var result = rendererWithScope(newScope, newOptions || parentOptions, parentNodeList || nodeList);
+                return result;
+            };
+            return observeObservables ? convertedRenderer : Observation.ignore(convertedRenderer);
+        },
+        getItemsStringContent: function (items, isObserveList, helperOptions, options) {
+            var txt = '';
+            for (var i = 0, len = isObserveList ? items.attr('length') : items.length; i < len; i++) {
+                txt += helperOptions.fn(isObserveList ? items.attr('' + i) : items[i], options);
+            }
+            return txt;
+        },
+        Options: Options
+    };
+});
