12af3f818f71a095c598cc5a789f288c4e3777ae
cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2017-06-30 14:42:12 +0100

    it works ready to merge

--- can-stache.js
+++ can-stache.js
@@ -249,7 +249,7 @@ function stache(template){
 						viewCallbacks.tagHandler(this,tagName, {
 							scope: scope,
 							options: options,
-							subtemplate: makeRendererConvertScopes(renderer),
+							subtemplate: renderer  ? makeRendererConvertScopes(renderer) : renderer,
 							templateType: "stache",
 							parentNodeList: parentNodeList,
 							templates: current.templates
