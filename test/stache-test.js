cf9f61732898355abd6eef56690af505b5e9e268
cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2017-10-09 19:57:09 +0100

    make test pass

--- test/stache-test.js
+++ test/stache-test.js
@@ -6273,24 +6273,21 @@ function makeTest(name, doc, mutation) {
 
 	test('check if <content> is already registred #165', function () {
 		stop();
-		var teardown = testHelpers.dev.willWarn(/Custom tag: content is already defined/, function(message, matched) {
-			QUnit.notOk(matched, message);
-		});
-		var handler = function(tag, tagData){
-		}
-		viewCallbacks.tag("content", handler);
-		clone({
-			'can-stache-bindings': {},
-			'can-util/js/dev/dev': {
-				warn: canDev.warn
-			}
-		})
-		.import('can-stache')
-		.then(function(stache){
-			stache('<content>foo</content>');
-			QUnit.equal(teardown(), 0, "Warning was not logged");
-			start();
-		});
+    viewCallbacks.tag("content", function() {});
+
+    var teardown = testHelpers.dev.willWarn(/Custom tag: content is already defined/, function(message, matched) {
+    	QUnit.notOk(matched, message);
+    });
+
+    clone({
+    	'can-view-callbacks': viewCallbacks
+    })
+    .import('can-stache')
+    .then(function(stache) {
+    	stache('<content>foo</content>');
+      QUnit.equal(teardown(), 0, "Warning was not logged");
+      start();
+    });
 	});
 
 	// PUT NEW TESTS RIGHT BEFORE THIS!
