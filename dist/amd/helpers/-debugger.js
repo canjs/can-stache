dc757d61905875babb4758e9d7076417890c3dc7
Kevin Phillips <kevin@bitovi.com>
2017-10-19 11:25:08 -0500

    3.10.3

---
+++ dist/amd/helpers/-debugger.js
@@ -0,0 +1,24 @@
+/*can-stache@3.10.2#helpers/-debugger*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-util/js/log'
+], function (require, exports, module) {
+    var canLog = require('can-util/js/log');
+    function noop() {
+    }
+    ;
+    var resolveValue = noop;
+    var evaluateArgs = noop;
+    var __testing = {};
+    function debuggerHelper(left, right) {
+        canLog.warn('Forgotten {{debugger}} helper');
+    }
+    module.exports = {
+        helper: debuggerHelper,
+        evaluateArgs: evaluateArgs,
+        resolveValue: resolveValue,
+        __testing: __testing
+    };
+});
