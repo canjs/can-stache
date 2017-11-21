0de542d2143e1ea879c4c935ae90f4dd9b1d1077
Kevin Phillips <kevin@bitovi.com>
2017-11-21 16:24:22 -0600

    3.14.2

---
+++ dist/amd/src/lookup-value-or-helper.js
@@ -0,0 +1,24 @@
+/*can-stache@3.14.1#src/lookup-value-or-helper*/
+define([
+    'require',
+    'exports',
+    'module',
+    './expression-helpers',
+    '../helpers/core'
+], function (require, exports, module) {
+    var expressionHelpers = require('./expression-helpers');
+    var mustacheHelpers = require('../helpers/core');
+    function lookupValueOrHelper(key, scope, helperOptions, readOptions) {
+        var scopeKeyData = expressionHelpers.getObservableValue_fromKey(key, scope, readOptions);
+        var result = { value: scopeKeyData };
+        if (key.charAt(0) === '@' && key !== '@index') {
+            key = key.substr(1);
+        }
+        if (scopeKeyData.initialValue === undefined || mustacheHelpers.helpers[key]) {
+            var helper = mustacheHelpers.getHelper(key, helperOptions);
+            result.helper = helper;
+        }
+        return result;
+    }
+    module.exports = lookupValueOrHelper;
+});
