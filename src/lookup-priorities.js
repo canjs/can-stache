6acc8e3155c8b3c13c695a26b218e282fa873b33
Kevin Phillips <kevin@bitovi.com>
2017-12-04 15:30:57 -0600

    simplifying lookup-priorities now that it is working

--- src/lookup-priorities.js
+++ src/lookup-priorities.js
@@ -1,16 +1,23 @@
-// order for what should take precendence when looking up
 // a helper or function in the scope
 // (local helpers most important, then built in helpers, etc)
-var priorities = [
-	"LOCAL_HELPER",
-	"BUILT_IN_HELPER",// #each helper
-	"SCOPE_FUNCTION",// DefineMap.prototype.each
-	"SCOPE_PROPERTY",
-	"GLOBAL_HELPER",
-	"MAX"
-].reduce(function(priorities, key, index) {
-	priorities[key] = index + 1;
-	return priorities;
-}, {});
+var priorities = {
+	// helpers in the templateContext
+	LOCAL_HELPER: 1,
+
+	// built-in helpers like #each
+	BUILT_IN_HELPER: 2,
+
+	// properties like DefineMap.prototype.each
+	SCOPE_FUNCTION: 3,
+
+	// properties on the scope object (that are not functions)
+	SCOPE_PROPERTY: 4,
+
+	// gloabl helpers registered by the user
+	GLOBAL_HELPER: 5,
+
+	// default priority
+	MAX: 6
+};
 
 module.exports = priorities;
