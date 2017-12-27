7701a507bb15af294967ef3b27dafc9ade553f92
Kevin Phillips <kevin@bitovi.com>
2017-12-27 13:53:33 -0600

    3.14.6

---
+++ dist/amd/src/html_section.js
@@ -0,0 +1,161 @@
+/*can-stache@3.14.5#src/html_section*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-view-target',
+    'can-view-scope',
+    'can-observation',
+    './utils',
+    './mustache_core',
+    'can-globals/document',
+    'can-util/js/assign',
+    'can-util/js/last'
+], function (require, exports, module) {
+    (function (global, require, exports, module) {
+        var target = require('can-view-target');
+        var Scope = require('can-view-scope');
+        var Observation = require('can-observation');
+        var utils = require('./utils');
+        var mustacheCore = require('./mustache_core');
+        var getDocument = require('can-globals/document');
+        var assign = require('can-util/js/assign');
+        var last = require('can-util/js/last');
+        var decodeHTML = typeof document !== 'undefined' && function () {
+            var el = getDocument().createElement('div');
+            return function (html) {
+                if (html.indexOf('&') === -1) {
+                    return html.replace(/\r\n/g, '\n');
+                }
+                el.innerHTML = html;
+                return el.childNodes.length === 0 ? '' : el.childNodes.item(0).nodeValue;
+            };
+        }();
+        var HTMLSectionBuilder = function (filename) {
+            if (filename) {
+                this.filename = filename;
+            }
+            this.stack = [new HTMLSection()];
+        };
+        HTMLSectionBuilder.scopify = function (renderer) {
+            return Observation.ignore(function (scope, options, nodeList) {
+                if (!(scope instanceof Scope)) {
+                    scope = Scope.refsScope().add(scope || {});
+                }
+                if (!(options instanceof mustacheCore.Options)) {
+                    options = new mustacheCore.Options(options || {});
+                }
+                return renderer(scope, options, nodeList);
+            });
+        };
+        assign(HTMLSectionBuilder.prototype, utils.mixins);
+        assign(HTMLSectionBuilder.prototype, {
+            startSubSection: function (process) {
+                var newSection = new HTMLSection(process);
+                this.stack.push(newSection);
+                return newSection;
+            },
+            endSubSectionAndReturnRenderer: function () {
+                if (this.last().isEmpty()) {
+                    this.stack.pop();
+                    return null;
+                } else {
+                    var htmlSection = this.endSection();
+                    return htmlSection.compiled.hydrate.bind(htmlSection.compiled);
+                }
+            },
+            startSection: function (process) {
+                var newSection = new HTMLSection(process);
+                this.last().add(newSection.targetCallback);
+                this.stack.push(newSection);
+            },
+            endSection: function () {
+                this.last().compile();
+                return this.stack.pop();
+            },
+            inverse: function () {
+                this.last().inverse();
+            },
+            compile: function () {
+                var compiled = this.stack.pop().compile();
+                return Observation.ignore(function (scope, options, nodeList) {
+                    if (!(scope instanceof Scope)) {
+                        scope = Scope.refsScope().add(scope || {});
+                    }
+                    if (!(options instanceof mustacheCore.Options)) {
+                        options = new mustacheCore.Options(options || {});
+                    }
+                    return compiled.hydrate(scope, options, nodeList);
+                });
+            },
+            push: function (chars) {
+                this.last().push(chars);
+            },
+            pop: function () {
+                return this.last().pop();
+            },
+            removeCurrentNode: function () {
+                this.last().removeCurrentNode();
+            }
+        });
+        var HTMLSection = function (process) {
+            this.data = 'targetData';
+            this.targetData = [];
+            this.targetStack = [];
+            var self = this;
+            this.targetCallback = function (scope, options, sectionNode) {
+                process.call(this, scope, options, sectionNode, self.compiled.hydrate.bind(self.compiled), self.inverseCompiled && self.inverseCompiled.hydrate.bind(self.inverseCompiled));
+            };
+        };
+        assign(HTMLSection.prototype, {
+            inverse: function () {
+                this.inverseData = [];
+                this.data = 'inverseData';
+            },
+            push: function (data) {
+                this.add(data);
+                this.targetStack.push(data);
+            },
+            pop: function () {
+                return this.targetStack.pop();
+            },
+            add: function (data) {
+                if (typeof data === 'string') {
+                    data = decodeHTML(data);
+                }
+                if (this.targetStack.length) {
+                    last(this.targetStack).children.push(data);
+                } else {
+                    this[this.data].push(data);
+                }
+            },
+            compile: function () {
+                this.compiled = target(this.targetData, getDocument());
+                if (this.inverseData) {
+                    this.inverseCompiled = target(this.inverseData, getDocument());
+                    delete this.inverseData;
+                }
+                this.targetStack = this.targetData = null;
+                return this.compiled;
+            },
+            removeCurrentNode: function () {
+                var children = this.children();
+                return children.pop();
+            },
+            children: function () {
+                if (this.targetStack.length) {
+                    return last(this.targetStack).children;
+                } else {
+                    return this[this.data];
+                }
+            },
+            isEmpty: function () {
+                return !this.targetData.length;
+            }
+        });
+        HTMLSectionBuilder.HTMLSection = HTMLSection;
+        module.exports = HTMLSectionBuilder;
+    }(function () {
+        return this;
+    }(), require, exports, module));
+});
---
+++ dist/amd/src/intermediate_and_imports.js
@@ -0,0 +1,95 @@
+/*can-stache@3.14.5#src/intermediate_and_imports*/
+define([
+    'require',
+    'exports',
+    'module',
+    './mustache_core',
+    'can-view-parser'
+], function (require, exports, module) {
+    var mustacheCore = require('./mustache_core');
+    var parser = require('can-view-parser');
+    module.exports = function (filename, source) {
+        if (arguments.length === 1) {
+            source = arguments[0];
+            filename = undefined;
+        }
+        var template = source;
+        template = mustacheCore.cleanWhitespaceControl(template);
+        template = mustacheCore.cleanLineEndings(template);
+        var imports = [], dynamicImports = [], ases = {}, inImport = false, inFrom = false, inAs = false, isUnary = false, importIsDynamic = false, currentAs = '', currentFrom = '';
+        function processImport() {
+            if (currentAs) {
+                ases[currentAs] = currentFrom;
+                currentAs = '';
+            }
+            if (importIsDynamic) {
+                dynamicImports.push(currentFrom);
+            } else {
+                imports.push(currentFrom);
+            }
+        }
+        var intermediate = parser(template, {
+            filename: filename,
+            start: function (tagName, unary) {
+                if (tagName === 'can-import') {
+                    isUnary = unary;
+                    importIsDynamic = false;
+                    inImport = true;
+                } else if (tagName === 'can-dynamic-import') {
+                    isUnary = unary;
+                    importIsDynamic = true;
+                    inImport = true;
+                } else if (inImport) {
+                    importIsDynamic = true;
+                    inImport = false;
+                }
+            },
+            attrStart: function (attrName) {
+                if (attrName === 'from') {
+                    inFrom = true;
+                } else if (attrName === 'as' || attrName === 'export-as') {
+                    inAs = true;
+                }
+            },
+            attrEnd: function (attrName) {
+                if (attrName === 'from') {
+                    inFrom = false;
+                } else if (attrName === 'as' || attrName === 'export-as') {
+                    inAs = false;
+                }
+            },
+            attrValue: function (value) {
+                if (inFrom && inImport) {
+                    currentFrom = value;
+                } else if (inAs && inImport) {
+                    currentAs = value;
+                }
+            },
+            end: function (tagName) {
+                if ((tagName === 'can-import' || tagName === 'can-dymamic-import') && isUnary) {
+                    processImport();
+                }
+            },
+            close: function (tagName) {
+                if (tagName === 'can-import' || tagName === 'can-dymamic-import') {
+                    processImport();
+                }
+            },
+            chars: function (text) {
+                if (text.trim().length > 0) {
+                    importIsDynamic = true;
+                }
+            },
+            special: function (text) {
+                importIsDynamic = true;
+            }
+        }, true);
+        return {
+            intermediate: intermediate,
+            imports: imports,
+            dynamicImports: dynamicImports,
+            ases: ases,
+            exports: ases
+        };
+    };
+});
