24a701d6674b4188f71a5ebc23d59d48c31c650b
Bradley Momberger <brad@bitovi.com>
2018-03-08 15:18:43 -0500

    Update dist for release

--- dist/amd/src/set-identifier.js
+++
@@ -1,6 +0,0 @@
-/*can-stache@3.14.10#src/set-identifier*/
-define(function (require, exports, module) {
-    module.exports = function SetIdentifier(value) {
-        this.value = value;
-    };
-});
